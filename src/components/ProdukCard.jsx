27acbce857ccba247b095c24ab740bf1d1645915
Muhammad Naufal amar <amarr140306@gmail.com>
2026-01-12 17:08:58 +0800

    menambah rating

--- src/components/ProdukCard.jsx
+++ src/components/ProdukCard.jsx
@@ -1,24 +1,20 @@
 import React, { useState } from "react";
 import formatIDR from "../services/format";
 import { ShoppingCart } from "lucide-react";
+import { Rating } from "react-simple-star-rating";
 
 export default function ProdukCard({ produk }) {
-
-    const[imgLoad, setLoad] = useState(false)
+  const [imgLoad, setLoad] = useState(false);
 
   return (
-    <div
-      className="h-110 rounded-xl overflow-hidden bg-white shadow-xl "
-    >
-      
+    <div className="h-110 rounded-xl overflow-hidden bg-white shadow-xl ">
       {/* gambar */}
       <div className="w-full h-52 ">
+        {/* skeleton load gambar */}
 
-      {/* skeleton load gambar */}
-
-      {!imgLoad && (
-        <div className="w-100 h-full bg-gray-200 animate-pulse"></div>
-      )}
+        {!imgLoad && (
+          <div className="w-100 h-full bg-gray-200 animate-pulse"></div>
+        )}
 
         <img
           src={produk.thumbnail}
@@ -34,13 +30,29 @@ export default function ProdukCard({ produk }) {
 
       {/* Deskripsi dan bintang */}
       <div className="px-5 mt-3">
-        <p className="w-70 font-mono text-xs line-clamp-2">{produk.description}</p>
+        <p className="w-70 font-mono text-xs line-clamp-2">
+          {produk.description}
+        </p>
+      </div>
+
+      {/* rating */}
+
+      <div className="flex items-center gap-2 px-5 mt-2">
+        <Rating
+          initialValue={produk.rating}
+          readonly={true}
+          allowFraction={true}
+          size={20}
+          SVGclassName="inline-block"
+          fillColor="oklch(39.3% 0.095 152.535)"
+        />
+        <span>{produk.rating}</span>
       </div>
       {/* button cart */}
-      <button className="border rounded-2xl p-2 font-family font-bold m-4 absolute transition-all delay-75 ease-in-out hover:bg-green-800 hover:text-white hover:cursor-pointer">
-        Add to chart
+      <button className="border rounded-2xl flex gap-2.5 p-2 text-xs items-center font-family font-bold m-4 absolute transition-all delay-75 ease-in-out hover:bg-green-800 hover:text-white hover:cursor-pointer">
+        <ShoppingCart className="text-xs" />
+        Masukkan Keranjang
       </button>
-      
     </div>
   );
 }
